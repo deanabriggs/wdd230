ccb22f0a46b9880ee66c81a13f506deafdfa25f4
deanabriggs <145316150+deanabriggs@users.noreply.github.com>
2024-04-30 00:43:30 -0700

    Added links and fixed errors

---
+++ week01/scripts/getDates.js
@@ -0,0 +1,5 @@
+// Copyright date
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+// Last Modified Date
+document.getElementById("lastModified").innerHTML = "Last Modified: " + new Date(document.lastModified);
