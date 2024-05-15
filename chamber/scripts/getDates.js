952e739276d57b81475c2330d90e8be238caf281
deanabriggs <145316150+deanabriggs@users.noreply.github.com>
2024-05-14 22:39:34 -0700

    wk3 chamber discover audit adjustments

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,2 +1,4 @@
 // Last Modified Date
-document.getElementById("lastModified").innerHTML = "Last Modified: " + new Date(document.lastModified);
+let modDate = new Date(document.lastModified)
+formattedDate = modDate.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
+document.getElementById("lastModified").innerHTML = "Last Modified: " + formattedDate;
