b7c751d61218e4945889b40068e18361000e7914
deanabriggs <145316150+deanabriggs@users.noreply.github.com>
2024-05-21 21:27:46 -0700

    Chamber - added aside to large view

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,4 +1,8 @@
 // Last Modified Date
-let modDate = new Date(document.lastModified)
+let modDate = new Date(document.lastModified);
 formattedDate = modDate.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
 document.getElementById("lastModified").innerHTML = "Last Modified: " + formattedDate;
+
+// Form Loaded Date
+let formDate = new Date(document.timestamp);
+document.getElementById("timestamp").innerHTML = formDate;
