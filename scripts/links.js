776bead62cac79cac75b0d55b79aa3839213258c
deanabriggs <145316150+deanabriggs@users.noreply.github.com>
2024-05-25 23:08:54 -0700

    wk5 update assignment portal with JSON file

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,38 @@
 const baseURL = "https://deanabriggs.github.io/wdd230/";
 const linksURL = "https://deanabriggs.github.io/wdd230/data/links.json";
+const assignments = document.querySelector("#assignments");
 
 async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log(data);
+    try {
+        const response = await fetch(linksURL);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayLinks(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const displayLinks = (weeks) => {
+
+    weeks.forEach((weekSet) => {
+        let weekName = document.createElement("li");
+        weekName.textContent = weekSet.week;
+
+        links.forEach((linkSet) => {
+            let link = document.createElement("a");
+            link.textContent = linkSet.title;
+            link.href = linkSet.url;
+            weekName.appendChild(link);
+        })
+
+        assignments.appendChild(weekName);
+    })
 }
 
 getLinks();
