30decd230a8c9ad6ed8864f92c61c92a93aa52e1
deanabriggs <145316150+deanabriggs@users.noreply.github.com>
2024-05-27 16:51:56 -0700

    correct members.json name

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,15 +1,15 @@
-const baseURL = "https://deanabriggs.github.io/wdd230/";
-const chamberMembersURL = "https://deanabriggs.github.io/wdd230/chamber/data/members.json";
+const baseURL = "https://deanabriggs.github.io/wdd230/chamber/";
+const membersURL = "https://deanabriggs.github.io/wdd230/chamber/data/members.json";
 
 const members = document.querySelector("#members");
 
-async function getLinks() {
+async function getMembers() {
     try {
-        const response = await fetch(linksURL);
+        const response = await fetch(membersURL);
         if (response.ok) {
             const data = await response.json();
-            console.log(data, nenbers);
-            displayLinks(data.members);
+            console.log(data);
+            // displayLinks(data.members);
         }
         else {
             throw Error(await response.text());
@@ -18,3 +18,5 @@ async function getLinks() {
         console.log(error);
     }
 }
+
+getMembers();
